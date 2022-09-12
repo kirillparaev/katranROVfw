305a1872063f214fadc4bdae9cdd2bbdf714b97f
Kirill Paraev <kir23rus2013@yandex.ru>
2022-09-12 11:47:44 +0300

    Added gyroscope functionality

--- katranROVfw.ino
+++ katranROVfw.ino
@@ -1,8 +1,12 @@
+#include <MPU6050.h>
 #include <SPI.h>
 #include <Servo.h>
 #include <Ethernet.h>
 #include <EthernetUDP.h>
+#include "I2Cdev.h"
+#include "Wire.h"
 
+#define BUFFER_SIZE 100
 Servo thruster[6];
 enum thrusters
 {
@@ -13,14 +17,14 @@ enum thrusters
     HORIZONTAL_2, //4
     HORIZONTAL_3  //5
 };
-
 //Servo th1, th2, th3, th4, th5, th6;
-
+MPU6050 GyroAccel;
+int16_t ax, ay, az;
+int16_t gx, gy, gz;
 byte mac[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED};
 IPAddress ip(192, 168,1,177);
 unsigned int localPort = 8888;
-
-char buffer[48];
+char buffer[49];
 //char buffer[UDP_TX_PACKET_MAX_SIZE] // 24 байта
 EthernetUDP udp;
 
@@ -28,7 +32,6 @@ void setup()
 {
   pinMode(LED_BUILTIN, OUTPUT);
   digitalWrite(53, OUTPUT);
-
   Ethernet.begin(mac, ip);
   
   for (int i = 0; i <= 5; i++) {
@@ -71,39 +74,87 @@ void setup()
   delay(1.5);
   */
   Serial.begin(9600);
+  Wire.begin();
+  delay(100);
 
   while (!Serial) 
   {
     ; // wait for serial port to connect. Needed for native USB port only
   }
 
-  if (Ethernet.hardwareStatus() == EthernetNoHardware) // Check for Ethernet hardware present
-  {
-    Serial.println("Ethernet shield was not found.  Sorry, can't run without hardware. :(");
-    while (true) 
-    {
-      delay(1); // do nothing, no point running without Ethernet hardware
-    }
-  }
-
-  if (Ethernet.linkStatus() == LinkOFF) 
-  {
-    Serial.println("Ethernet cable is not connected.");
-  }
-
   udp.begin(localPort);
+  calibration();
 }
 
 void loop() 
 {
     int packetSize = udp.parsePacket();
     if (packetSize) {
-        udp.read(buffer, 48);
+        udp.read(buffer, 49);
         Serial.println("Contents:");
         Serial.println(buffer);
+        if (buffer[48] == 0xFF) {
+            calibration();
+        }
     }
     for (int i = 0; i <= 5; i++) {
         thruster[i].write(buffer[42 + i]); // данные в пакете начинаются с 42 байта
     }
     delay(15);  // waits for the servo to get there
+
+    // TODO : вынести в отдельную функцию и заставить работать постоянно
+    // расшифровать сырые данные
+    // пересмотреть структуру udp пакета для отправки данных наверх
+    // прикрутить калибровку и команду на неё сверху
+    GyroAccel.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+    Serial.print(ax); Serial.print("\t");
+    Serial.print(ay); Serial.print("\t");
+    Serial.print(az); Serial.print("\t");
+    Serial.print(gx); Serial.print("\t");
+    Serial.print(gy); Serial.print("\t");
+    Serial.println(gz);
+}
+
+void calibration() {
+    long offsets[6];
+    long offsetsOld[6];
+    int16_t mpuGet[6];
+    // используем стандартную точность
+    GyroAccel.setFullScaleAccelRange(MPU6050_ACCEL_FS_2);
+    GyroAccel.setFullScaleGyroRange(MPU6050_GYRO_FS_250);
+    // обнуляем оффсеты
+    GyroAccel.setXAccelOffset(0);
+    GyroAccel.setYAccelOffset(0);
+    GyroAccel.setZAccelOffset(0);
+    GyroAccel.setXGyroOffset(0);
+    GyroAccel.setYGyroOffset(0);
+    GyroAccel.setZGyroOffset(0);
+    delay(10);
+    Serial.println("Calibration start. It will take about 5 seconds");
+    for (byte n = 0; n < 10; n++) {     // 10 итераций калибровки
+        for (byte j = 0; j < 6; j++) {    // обнуляем калибровочный массив
+            offsets[j] = 0;
+        }
+        for (byte i = 0; i < 100 + BUFFER_SIZE; i++) { // делаем BUFFER_SIZE измерений для усреднения
+            GyroAccel.getMotion6(&mpuGet[0], &mpuGet[1], &mpuGet[2], &mpuGet[3], &mpuGet[4], &mpuGet[5]);
+            if (i >= 99) {                         // пропускаем первые 99 измерений
+                for (byte j = 0; j < 6; j++) {
+                    offsets[j] += (long)mpuGet[j];   // записываем в калибровочный массив
+                }
+            }
+        }
+        for (byte i = 0; i < 6; i++) {
+            offsets[i] = offsetsOld[i] - ((long)offsets[i] / BUFFER_SIZE); // учитываем предыдущую калибровку
+            if (i == 2) offsets[i] += 16384;                               // если ось Z, калибруем в 16384
+            offsetsOld[i] = offsets[i];
+        }
+        // ставим новые оффсеты
+        GyroAccel.setXAccelOffset(offsets[0] / 8);
+        GyroAccel.setYAccelOffset(offsets[1] / 8);
+        GyroAccel.setZAccelOffset(offsets[2] / 8);
+        GyroAccel.setXGyroOffset(offsets[3] / 4);
+        GyroAccel.setYGyroOffset(offsets[4] / 4);
+        GyroAccel.setZGyroOffset(offsets[5] / 4);
+        delay(2);
+    }
 }
