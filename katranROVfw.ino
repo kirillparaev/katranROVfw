961ab99bc959069932beec83fe007277cfbc72ad
Kirill Paraev <kir23rus2013@yandex.ru>
2023-03-09 12:44:11 +0300

    master version

--- katranROVfw.ino
+++ katranROVfw.ino
@@ -7,29 +7,43 @@
 #include <Servo.h>
 #include <Ethernet.h>
 #include <EthernetUDP.h>
+#include <ArduinoOTA.h>
 #include "I2Cdev.h"
 #include "Wire.h"
 
+#define BRAKEVCC 0
+#define CW   1
+#define CCW  2
+#define BRAKEGND 3
+#define CS_THRESHOLD 100
 #define BUFFER_SIZE_CALIBRATION 100
+
 Servo thruster[6];
 enum thrusters
 {
-    VERTICAL_0, //0
-    VERTICAL_1, //1
-    HORIZONTAL_0, //2
-    HORIZONTAL_1, //3
-    HORIZONTAL_2, //4
-    HORIZONTAL_3  //5
+    VERTICAL_FRONT,
+    VERTICAL_BACK,
+    HORIZONTAL_FRONT_RIGHT,
+    HORIZONTAL_BACK_RIGHT, 
+    HORIZONTAL_BACK_LEFT, 
+    HORIZONTAL_FRONT_LEFT 
 };
+
+/*  VNH2SP30 pin definitions
+ xxx[0] controls '1' outputs
+ xxx[1] controls '2' outputs */
+int inApin[2] = { 3, 10 };  // INA: Clockwise input
+int inBpin[2] = { 4, 6 }; // INB: Counter-clockwise input
+int ENpin[2] = { 5, 7 }; // PWM input
+
 MPU6050 GyroAccel;
 MS5837 barometer;
 int16_t ax, ay, az;
-//избавиться от глобальных переменных
 int16_t gx, gy, gz;
 byte mac_local[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED};
 IPAddress ip_local(192,168,0,177);
 unsigned int port_local = 8080;
-char packetBuffer[UDP_TX_PACKET_MAX_SIZE]; // 24 байта
+char packetBuffer[UDP_TX_PACKET_MAX_SIZE];
 EthernetUDP udp;
 AF_DCMotor manipulator_0(1);
 AF_DCMotor manipulator_1(2);
@@ -41,21 +55,39 @@ void setup()
   Ethernet.init(10);
   Ethernet.begin(mac_local, ip_local);
   
-  for (int i = 0; i <= 5; i++) {  // pins 3-8
-      thruster[i].attach(i + 3);
-  }
+  thruster[VERTICAL_FRONT].attach(8);
+  thruster[VERTICAL_BACK].attach(9);
+  thruster[HORIZONTAL_FRONT_RIGHT].attach(11);
+  thruster[HORIZONTAL_BACK_RIGHT].attach(13);
+  thruster[HORIZONTAL_BACK_LEFT].attach(2);
+  thruster[HORIZONTAL_FRONT_LEFT].attach(12);
 
   for (int i = 0; i <= 5; i++) {
       thruster[i].write(180);
+      delay(2);
+      thruster[i].write(90);
+      delay(1.5);
   }
 
-  delay(2);
+  /*delay(2);
   for (int i = 0; i <= 5; i++) {
       thruster[i].write(90);
   }
-  delay(1.5);
+  delay(1.5);*/
   Serial.begin(9600);
   Wire.begin();
+  for (int i = 0; i < 2; i++)
+  {
+      pinMode(inApin[i], OUTPUT);
+      pinMode(inBpin[i], OUTPUT);
+      pinMode(ENpin[i], OUTPUT);
+  }
+  // Initialize braked
+  for (int i = 0; i < 2; i++)
+  {
+      digitalWrite(inApin[i], LOW);
+      digitalWrite(inBpin[i], LOW);
+  }
   if (!barometer.begin()) {
       Serial.println("Init failed!");
       Serial.println("Are SDA/SCL connected correctly?");
@@ -64,10 +96,10 @@ void setup()
   }
   else {
       Serial.println("Barometer has initialized!!!");
-      barometer.setFluidDensity(997); // kg/m^3 (freshwater, 1029 for seawater)В;
+      barometer.setModel(MS5837::MS5837_30BA);
+      barometer.setFluidDensity(997); // kg/m^3 (freshwater, 1029 for seawater);
   }
-  barometer.setModel(MS5837::MS5837_30BA);
- 
+  
   while (!Serial) 
   {
     ; // wait for serial port to connect. Needed for native USB port only
@@ -78,14 +110,17 @@ void setup()
   Serial.print(ip_local);
   Serial.print(":");
   Serial.println(port_local);
+  ArduinoOTA.begin(ip_local, "Arduino", "password", InternalStorage);
   //calibration();
 }
 
 void loop() 
 {
+    ArduinoOTA.poll();
+    // check for updates
     int packetSize = udp.parsePacket();
+    // получение данных ПЕРЕД пакетом
     //updateBarometerReadings();
-
     if (packetSize) {
         Serial.print("Remote PC IP and port: ");
         Serial.print(udp.remoteIP());
@@ -96,34 +131,21 @@ void loop()
 
         udp.read(packetBuffer, UDP_TX_PACKET_MAX_SIZE);
         Serial.println("Contents:");
-        //for (int i = 0; i <= 6; i++) {
-        //    packetBuffer[i] = atoi(&packetBuffer[i]);    
-        //}
         Serial.println(packetBuffer);
-
-        if (packetBuffer[6] == 0xFF) {
-            calibration();
-        }
-        manipulatorActions(packetBuffer[7], packetBuffer[8]);
     }
-
+    // отправка данных ПОСЛЕ получения нового пакета
     for (int i = 0; i <= 5; i++) {
-        thruster[i].write(packetBuffer[0 + i]);
+        thruster[i].write(packetBuffer[i]);
     }
+    manipulatorActions(packetBuffer[6], packetBuffer[7]);
 
-    delay(15);  // waits for the servo to get there
-
-    // TODO : вынести в отдельную функцию и заставить работать постоянно
-    // расшифровать сырые данные
-    // пересмотреть структуру udp пакета для отправки данных наверх
-    // прикрутить калибровку и команду на неё сверxy
     GyroAccel.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
-    /*Serial.print(ax); Serial.print("\t");
+    Serial.print(ax); Serial.print("\t");
     Serial.print(ay); Serial.print("\t");
     Serial.print(az); Serial.print("\t");
     Serial.print(gx); Serial.print("\t");
     Serial.print(gy); Serial.print("\t");
-    Serial.println(gz);*/
+    Serial.println(gz);
 }
 
 void calibration() {
@@ -193,57 +215,74 @@ void updateBarometerReadings() {
     delay(1000);
 }
 
-void manipulatorActions(int cgrip, int crotate) { // 0-255
-    float val_grip = fabs(cgrip);
-    float val_rotate = fabs(crotate);
-    int sign_grip, sign_rotate;
+void motorOff(int motor)
+{
+    // Initialize braked
+    for (int i = 0; i < 2; i++)
+    {
+        digitalWrite(inApin[i], LOW);
+        digitalWrite(inBpin[i], LOW);
+    }
+    analogWrite(ENpin[motor], 0);
+}
 
-    if (val_grip != 0) {
-        sign_grip = val_grip / cgrip;
+/* motorGo() will set a motor going in a specific direction
+ the motor will continue going in that direction, at that speed
+ until told to do otherwise.
+
+ motor: this should be either 0 or 1, will selet which of the two
+ motors to be controlled
+
+ direct: Should be between 0 and 3, with the following result
+ 0: Brake to VCC
+ 1: Clockwise
+ 2: CounterClockwise
+ 3: Brake to GND
+
+ pwm: should be a value between ? and 1023, higher the number, the faster
+ it'll go
+ */
+void motorGo(uint8_t motor, uint8_t direct, uint8_t pwm)
+{
+    if (motor <= 1)
+    {
+        if (direct <= 4)
+        {
+            // Set inA[motor]
+            if (direct <= 1)
+                digitalWrite(inApin[motor], HIGH);
+            else
+                digitalWrite(inApin[motor], LOW);
+
+            // Set inB[motor]
+            if ((direct == 0) || (direct == 2))
+                digitalWrite(inBpin[motor], HIGH);
+            else
+                digitalWrite(inBpin[motor], LOW);
+
+            analogWrite(ENpin[motor], pwm);
+        }
+    }
+}
+
+void manipulatorActions(int cgrip, int crotate) {
+   if (crotate == 0) {
+       motorGo(0, CCW, 1023);
+    }
+    else if (crotate == 2) {
+       motorGo(0, CW, 1023);
     }
     else {
-        sign_grip = 0;
+        motorOff(0);
     }
 
-    if (val_rotate != 0) {
-        sign_rotate = val_rotate / crotate;
+    if (cgrip == 0) {
+        motorGo(1, CCW, 1023);
+    }
+    else if (cgrip == 2) {
+        motorGo(1, CW, 1023);
     }
     else {
-        sign_rotate = 0;
+        motorOff(1);
     }
-
-    if (sign_grip >= 0) { // 1 - на мотор шилде
-        manipulator_0.run(FORWARD);
-    }
-    else {
-        manipulator_0.run(BACKWARD);
-    }
-    manipulator_0.setSpeed(val_grip);
-
-    /*if (sign_rotate >= 0) { //  - на мотор шилде
-        manipulator_1.run(FORWARD);
-    }
-    else {
-        manipulator_1.run(BACKWARD);
-    }
-    manipulator_1.setSpeed(val_rotate);*/
-
-
-    //-------------------------------------------
-    /*for (int i = 0; i < 255; i++) {
-        manipulator_0.run(FORWARD);
-        manipulator_0.setSpeed(i);
-    }
-    for (int i = 0; i < 255; i++) {
-        manipulator_0.run(BACKWARD);
-        manipulator_0.setSpeed(i);
-    }
-    for (int i = 0; i < 255; i++) {
-        manipulator_1.run(FORWARD);
-        manipulator_1.setSpeed(i);
-    }
-    for (int i = 0; i < 255; i++) {
-        manipulator_1.run(BACKWARD);
-        manipulator_1.setSpeed(i);
-    }*/
 }
